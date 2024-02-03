f62c2add609f0062b74b485fe6b36cda0ad91b73
Chanhaeng Lee <lch32111@naver.com>
2024-02-03 11:00:19 -0500

    changed build.bat for 4coder in order to change code easily while building in github

--- code/bin/build.bat
+++ code/bin/build.bat
@@ -20,6 +20,9 @@ set opts=%opts% /GR- /EHa- /nologo /FC /Zi
 set opts=%opts% /I%src_root% /I%custom_root%
 set opts=%opts% %mode%
 
+REM NOTE(chan) : change the output of the cl
+set opts=%opts% /Fo%build_root%
+
 set FirstError=0
 pushd %build_root%
 call cl %opts% kernel32.lib %bin_root%\4ed_build.cpp /Febuild
@@ -27,6 +30,19 @@ if %ERRORLEVEL% neq 0 (set FirstError=1)
 if %ERRORLEVEL% neq 0 (goto END)
 popd
 
+REM NOTE(chan) : I don't know why build.obj is not created on build folder
+REM so I move manually here
+move %src_root%\..\build.obj %build_root%
+
+REM NOTE(chan) : code\custom\generated\command_metadata.h is changed in compiling.
+REM so I copy the original one temporarily and paste it to the changed one after building the program.
+REM this helps to remove boring jobs for git code changes
+copy %src_root%\custom\generated\command_metadata.h %src_root%\..\original_command_metadata.h
+
 %build_root%\build
+
+REM The building is done. Let's overwrite the changed metadata.h with the original one
+move %src_root%\..\original_command_metadata.h %src_root%\custom\generated\command_metadata.h
+
 :END
 if %ERRORLEVEL% neq 0 (set FirstError=1)
